544b782ed0a1777747fbc50db637fd6b94bb8472
Németh Balázs <nemeth.balazs.19f@szlgbp.com>
2022-05-23 10:57:23 +0200

    database 2

--- APP/models.py
+++ APP/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import User
 
 class Game_data(models.Model):
     class Meta:
@@ -7,10 +7,7 @@ class Game_data(models.Model):
         verbose_name = 'game_data'
         verbose_name_plural = 'game_datas'
 
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        to_field='username')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     cookie_number = models.BigIntegerField(default=0)
     grandma_number = models.BigIntegerField(default=0)
     factory_number = models.BigIntegerField(default=0)
@@ -19,7 +16,6 @@ class Game_data(models.Model):
     click_upgrade_3 = models.BooleanField(default=False)
     click_upgrade_4 = models.BooleanField(default=False)
     level = models.BigIntegerField(default=1)
-
 
     def __str__(self):
         return f"{self.user}, {self.level}"
--- APP/views.py
+++ APP/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Game_data
 
 @login_required
 def index(request):
     template='index.html'
-    context={}
+    
+    context={
+        "cookies" : request.user.game_data.cookie_number,
+        "grandmas" : request.user.game_data.grandma_number,
+        "factories" : request.user.game_data.factory_number,
+        "click_upgrade_1" : request.user.game_data.click_upgrade_1,
+        "click_upgrade_1" : request.user.game_data.click_upgrade_2,
+        "click_upgrade_1" : request.user.game_data.click_upgrade_3,
+        "click_upgrade_1" : request.user.game_data.click_upgrade_4,
+        "level" : request.user.game_data.level,
+    }
     return render(request, template, context)
