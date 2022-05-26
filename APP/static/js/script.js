c5225cd644508fc0a9346b3653b34789b8272bfb
szabo bence <szabo.bence.19f@szlgbp.hu>
2022-05-26 14:16:06 +0200

    js

---
+++ APP/static/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", main)
+
+function main() {
+    gomb = document.querySelector("#bután");
+    gomb.addEventListener("click", function() {
+        alert('asd');
+    });
+    
+}
